e13da48e187d00c6d0394ebc512cbc4f69b80db5
daanielsb <daanielsb@hotmail.com>
2023-07-24 01:20:30 -0300

    variant colors added

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Button } from "./components/Button"
 export function App() {
   return (
     <>
-      <Button color="secondary"/>
-      <Button color="secondary"/>
-      <Button color="sucess"/>
-      <Button />
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="sucess"/>
+      <Button variant="danger"/>
+      <Button/>
+      
     </>
   )}
 
