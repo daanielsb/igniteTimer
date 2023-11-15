6523734357fe5255c2abb2c3cdfacae73843cd05
daanielsb <daanielsb@hotmail.com>
2023-11-15 16:12:46 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from 'styled-components'
-import { HashRouter } from 'react-router-dom'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Router } from './Router'
@@ -8,11 +7,9 @@ import { CyclesContextProvider } from './contexts/CyclesContext'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <HashRouter>
-        <CyclesContextProvider>
-          <Router />
-        </CyclesContextProvider>
-      </HashRouter>
+      <CyclesContextProvider>
+        <Router />
+      </CyclesContextProvider>
       <GlobalStyle />
     </ThemeProvider>
   )
